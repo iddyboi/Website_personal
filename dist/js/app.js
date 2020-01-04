30b877fe1497bf58a3587bcceb3d5ff100cf05cc
Idris <idrist2013@gmail.com>
2020-01-04 18:30:21 +0000

    server creation

---
+++ dist/js/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv/config");
+
+// import routes
+
+const postsRoutes = require("./routes/post");
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
+
+app.use("/post", postsRoutes);
+
+// connect to db
+
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
+  console.log("connected to db");
+});
---
+++ dist/models/Post.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const PostSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: String,
+  date: Date.now
+});
+
+module.exports = mongoose.model("Posts", PostSchema);
